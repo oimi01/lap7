acddb17a028d21df6233e49b7881db8b88ebe346
kirakorn <apikorn.111@gamil.com>
2023-12-12 13:19:42 +0700

    complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,18 +2,14 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if( x >= 66 && x <= 90)
+	{
+		return x-1;
+	}
+	else if ( x == 65)
+	{
+		return 'Z';
+	}
+	else return '0';
 }
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
-}
